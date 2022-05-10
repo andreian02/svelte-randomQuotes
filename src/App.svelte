862c45f7b79304a2dc5e5b63db3a5433e9b8a540
Andrew <andrew.lim@tbwa.com>
2022-05-10 13:54:32 +0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,59 @@
+<script>
+	import Quote from "../src/lib/components/quotes.svelte";
+	export let name;
+
+
+</script>
+
+
+<main>
+	<h1>Hello {name}!</h1>
+</main>
+<div class="mx-auto flex max-w-2xl flex-col items-start justify-center">
+	<section class="flex flex-col-reverse items-start sm:flex-row">
+		<div class="container">
+			<h3 class="mb-6 text-2xl font-bold tracking-tight md:text-4xl">
+				Random Quotes
+			</h3>
+		</div>
+
+		<div class="container">
+			<div>
+				<Quote />
+			</div>
+		</div>
+	</section>
+</div>
+
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	.container {
+		margin-left: 100px;
+		margin-right: 100px;
+
+	}
+
+	h1 {
+		color: rebeccapurple;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	h3 {
+		font-weight: 400;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/lib/components/quotes.svelte
@@ -0,0 +1,47 @@
+<script>
+	import data from '../../assets/quotes.json';
+
+	import { fade } from 'svelte/transition';
+	let options = { duration: 400 };
+
+	export let quotes = data;
+	let quote = getRandomQuote(quotes);
+
+	function getRandomQuote(quotes) {
+		return quotes[Math.floor(Math.random() * quotes.length)];
+	}
+
+	function updateQuote() {
+		quote = getRandomQuote(quotes);
+	}
+</script>
+
+<div class="box">
+	<blockquote>
+		{#key quote}
+			<div in:fade={options}>
+				<p class="quote content">{quote.quote}</p>
+				<footer> {quote.author}</footer>
+			</div>
+		{/key}
+	</blockquote>
+</div>
+
+<button on:click={updateQuote} type="submit">
+	<span class="icon"><i class="fas fa-redo" /></span>
+	<span>Generate Quote</span>
+</button>
+
+<style>
+	footer {
+        
+		font-weight: 500;
+		margin-left: 3rem;
+	}
+	footer::before {
+		content: '\2014 ';
+	}
+	blockquote {
+		margin-bottom: 2rem;
+	}
+</style>
